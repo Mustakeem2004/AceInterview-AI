4dc50cba80deb9993626ae0214fefa50fbbd87a8
Mustakeem <00786malikmustakeem@gmail.com>
2026-05-18 23:12:10 +0530

    inital commit

---
+++ server/config/connectDb.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDb = async () =>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("Database connected");
+        
+    }
+    catch(error){
+        console.log(`Database connecting error ${error}`);
+        
+    }
+}
+
+export default connectDb
---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDb from "./config/connectDb.js"
+dotenv.config()
+const app=express()
+
+
+const PORT = process.env.PORT || 3000
+app.get("/",(req,res)=>{
+    return res.json({"message":"server started"})
+    });
+
+app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`);
+    connectDb()
+})
